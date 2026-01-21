ce048e9c163ef92cc4dea7684279d6ce8594e2c9
yousentech <83219678+yousentech@users.noreply.github.com>
2026-01-21 16:48:46 +0300

    update

--- models/account_move.py
+++ models/account_move.py
@@ -33,5 +33,48 @@ class AccountMove(models.Model):
                 raise ValidationError(
                     _('You cannot mix multiple Operation Units in one invoice.')
                 )
+ 
+    
+    def _sync_ou_from_sale_order(self):
+        for move in self:
+            sale_orders = move.invoice_line_ids.mapped(
+                'sale_line_ids.order_id'
+            ).filtered(lambda x: x)
 
-                
+            if len(sale_orders) != 1:
+                continue
+
+            sale = sale_orders[0]
+            ou = sale.operation_unit_id
+
+            if not ou:
+                continue
+            # لو الفاتورة OU فاضي → نملأه
+            move.write({
+                'operation_unit_id': sale.operation_unit_id.id
+            })
+            move.line_ids.write({
+                'operation_unit_id': sale.operation_unit_id.id
+            })
+
+            # لو عليها دفعات → نزامن الدفعات
+            move._sync_ou_to_payments()
+
+    def _sync_ou_to_payments(self):
+        for move in self:
+            payments = move.line_ids.mapped(
+                'matched_debit_ids.move_id'
+            ) | move.line_ids.mapped(
+                'matched_credit_ids.move_id'
+            )
+
+            payments = payments.filtered(lambda m: m.payment_id)
+
+            for pay_move in payments:
+                payment = pay_move.payment_id
+                if not payment.operation_unit_id:
+                    payment.write({
+                        'operation_unit_id': move.operation_unit_id.id
+                    })
+
+
